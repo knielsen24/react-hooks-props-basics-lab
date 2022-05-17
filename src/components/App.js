499d9fd56df94a02330cdec6b49a23f49cc079e0
Kevin Nielsen <knielsen24@gmail.com>
2022-05-16 17:37:38 -0700

    commit tree 51a50d2a32bc8cec50204b6095abcc51ba7ceef1

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
-  return (
+	const { bio, color, name, city, } = user
+	const { github, linkedin} = user.links
+
+	return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={color} name={name} city={city}/>
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
-  );
+  	);
 }
 
 export default App;
